926c486d4e64f62ef4b56e375f9fef7eb63bd40d
Romit  Dey <romit77dey@gmail.com>
2024-02-17 22:55:13 +0530

    added icons

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,12 @@
+import { FaFileInvoice } from "react-icons/fa6";
+
 function Card() {
   return (
-    <div>
-      <div className="w-60 h-72 rounded-[20px] bg-zinc-900"></div>
+    <div className="w-60 h-72 rounded-[20px] bg-zinc-900 text-white p-5">
+      <FaFileInvoice />
+      <p className="text-sm font-medium mt-5 leading-tight">
+        Lorem ipsum, dolor sit amet consectetur adipisicing.
+      </p>
     </div>
   );
 }
